50df6e660857f62e2cae7c4501d5a3b16074c5e9
Alejandro-Ogando <aogando09@gmail.com>
2021-10-02 18:43:13 -0400

    Changes to config

--- FlightApp/Services/FlightApiServices.cs
+++ FlightApp/Services/FlightApiServices.cs
@@ -9,21 +9,21 @@ using Newtonsoft.Json;
 
 namespace FlightApp.Services
 {
-        public class FlightApiServices : IFlightApiServices
-        {
-        const string ApiAccessKey = "54584c8325755dc2dbad7aadad6df16a";
-        public async Task<FlightResponse> GetFlightInfoAsync()
-            {
-                HttpClient httpClient = new HttpClient();
-                var response = await httpClient.GetAsync($"https://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&limit=30&show_to_affiliates=true&sorting=price&trip_class=0&token=" + ApiAccessKey);
+    public class FlightApiServices : IFlightApiServices
+    {
 
+        public async Task<FlightResponse> GetFlightInfoAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(Config.ApiUrl);
+            
             if (response.IsSuccessStatusCode)
-                {
-                    var responseString = await response.Content.ReadAsStringAsync();
-                    var flightResponse = JsonConvert.DeserializeObject<FlightResponse>(responseString);
-                    return flightResponse;
-                }
-                return null;
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var flightResponse = JsonConvert.DeserializeObject<FlightResponse>(responseString);
+                return flightResponse;
             }
+            return null;
         }
+    }
 }
