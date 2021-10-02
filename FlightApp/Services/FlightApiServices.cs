bb201629dd6f76a3166ccbea239c78e58f93e31f
juan2305 <78822907+juan2305@users.noreply.github.com>
2021-10-02 13:27:37 -0400

    Code Changes 2

--- FlightApp/Services/FlightApiServices.cs
+++ FlightApp/Services/FlightApiServices.cs
@@ -11,13 +11,13 @@ namespace FlightApp.Services
 {
         public class FlightApiServices : IFlightApiServices
         {
-           
-            public async Task<FlightResponse> GetFlightInfoAsync()
+        const string ApiAccessKey = "54584c8325755dc2dbad7aadad6df16a";
+        public async Task<FlightResponse> GetFlightInfoAsync()
             {
                 HttpClient httpClient = new HttpClient();
-                var response = await httpClient.GetAsync(Config.ApiUrl);
+                var response = await httpClient.GetAsync($"https://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&limit=30&show_to_affiliates=true&sorting=price&trip_class=0&token=" + ApiAccessKey);
 
-                if (response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
                 {
                     var responseString = await response.Content.ReadAsStringAsync();
                     var flightResponse = JsonConvert.DeserializeObject<FlightResponse>(responseString);
---
+++ FlightApp/ViewModels/FlightViewModel.cs
@@ -0,0 +1,34 @@
+﻿using FlightApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using static FlightApp.Models.FlightDatum;
+
+namespace FlightApp.ViewModels
+{
+    public class FlightViewModel : BaseViewModel
+    {
+        public string Value { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Gate { get; set; }
+        public string Distance { get; set; }
+        public ICommand DeployDataCommand { get; set; }
+        public List<Datum> Data { get; set; }
+
+        public FlightViewModel()
+        {
+            _flightApiService = new FlightApiServices();
+            DeployDataCommand = new Command(OnDeploy);
+        }
+
+        private async void OnDeploy()
+        {
+            var flightResponse = await _flightApiService.GetFlightInfoAsync();
+            this.Data = flightResponse.Data;
+        }
+        IFlightApiServices _flightApiService;
+    }
+}
